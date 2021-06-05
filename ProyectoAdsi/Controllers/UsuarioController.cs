e743ac48a7cdf4126441a0048ba609a3ee91381b
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-05 17:21:14 -0500

    Colsulta datos tabla usuario

---
+++ ProyectoAdsi/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAdsi.Models;
+
+
+namespace ProyectoAdsi.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+    }
+     
+    
+}
