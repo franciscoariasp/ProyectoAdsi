946c8d973ba57999c37ad26368a538993d5f6724
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-07 15:08:49 -0500

    Creando "Index"Tabla Roles

---
+++ ProyectoAdsi/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAdsi.Models;
+
+namespace ProyectoAdsi.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.roles.ToList());
+            }
+        }
