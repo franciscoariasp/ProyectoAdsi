0f3fec548e92e6382a18b7e64376c693a3945fe5
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-07 15:16:02 -0500

    Se  agrera"Create"Tabla Roles

--- ProyectoAdsi/Controllers/RolesController.cs
+++ ProyectoAdsi/Controllers/RolesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoAdsi.Models;
@@ -16,6 +17,49 @@ namespace ProyectoAdsi.Controllers
             {
                 return View(db.roles.ToList());
             }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(roles roles)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+                {
+                    roles.password = RolesController.HashSHA1(roles.password);
+                    db.roles.Add(roles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
