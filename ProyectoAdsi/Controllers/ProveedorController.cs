815c9d73437c1261081336b26a9393c442086dbf
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-10 23:01:58 -0500

    Correcion Crud"Proveedor"

--- ProyectoAdsi/Controllers/ProveedorController.cs
+++ ProyectoAdsi/Controllers/ProveedorController.cs
@@ -45,19 +45,8 @@ namespace ProyectoAdsi.Controllers
                 return View();
             }
         }
-        public static string HashSHA1(string value)
-        {
-            var sha1 = System.Security.Cryptography.SHA1.Create();
-            var inputBytes = Encoding.ASCII.GetBytes(value);
-            var hash = sha1.ComputeHash(inputBytes);
-
-            var sb = new StringBuilder();
-            for (var i = 0; i < hash.Length; i++)
-            {
-                sb.Append(hash[i].ToString("X2"));
-            }
-            return sb.ToString();
-        }
+        
+        
 
         public ActionResult Edit(int id)
         {
