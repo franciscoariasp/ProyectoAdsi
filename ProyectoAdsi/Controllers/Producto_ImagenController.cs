21e819db4c61ff00222488d8a4a8e927e349ebd3
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-14 22:29:27 -0500

    Creando Crud"Producto_Imagen"

---
+++ ProyectoAdsi/Controllers/Producto_ImagenController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAdsi.Models,
+
+namespace ProyectoAdsi.Controllers
+{
+    public class Producto_ImagenController : Controller
+    {
+        // GET: Producto_Imagen
+        public ActionResult Index()
+
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_imagen.ToList());
+            }
+
+        }
+        public static string NombreProducto(int idProducto)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto_imagen newProductoImagen)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto_imagen.Add(newProductoImagen);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+
+        {
+            using (var db = new inventario2021Entities())
+
+            {
+                producto_imagen producto_imagenDetalle = db.producto_imagen.Where(a => a.id == id).FirstOrDefault();
+                return View(producto_imagenDetalle);
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            using (var db = new inventario2021Entities())
+            {
+                var product_imagenDelete = db.producto_imagen.Find(id);
+                db.producto_imagen.Remove(product_imagenDelete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+
+                {
+                    producto_imagen productoImagen = db.producto_imagen.Where(a => a.id == id).FirstOrDefault();
+                    return View(productoImagen);
+                }
+
+            }
+            catch (Exception ex)
+
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+
+            }
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto_imagen productoImagenEdit)
+
+        {
+            try
+
+            {
+
+                using (var db = new inventario2021Entities())
+
+                {
+                    var producto_imagen = db.producto_imagen.Find(productoImagenEdit.id);
+                    producto_imagen.imagen = productoImagenEdit.imagen;
+                    producto_imagen.id_producto = productoImagenEdit.id_producto;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+
+    }
+}
---
+++ ProyectoAdsi/Controllers/Usuario_RolController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAdsi.Models;
+
+
+namespace ProyectoAdsi.Controllers
+{
+    public class Usuario_RolController : Controller
+    {
+        // GET: UsuarioRol
+        public ActionResult Index()
+
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+
+        }
+
+        public static string UsuarioRol(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuarios()
+        {
+            using (var db = new inventario2021Entities())
+
+            {
+                return PartialView(db.usuario.ToList());
+
+            }
+        }
+
+        public static string RolUsuario(int idRole)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.roles.Find(idRole).descripcion;
+            }
+        }
+
+        public ActionResult ListarRoles()
+        {
+            using (var db = new inventario2021Entities())
+
+            {
+                return PartialView(db.roles.ToList());
+
+            }
+        }
+
+        public ActionResult Create()
+
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuariorol usuarioRol)
+
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+
+                {
+                    db.usuariorol.Add(usuarioRol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var usuarioRol = db.usuariorol.Find(id);
+                db.usuariorol.Remove(usuarioRol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol finduser = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(finduser);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuariorol usuarioRolEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol usuarioRol = db.usuariorol.Find(usuarioRolEdit.id);
+
+                    usuarioRol.idUsuario = usuarioRolEdit.idUsuario;
+                    usuarioRol.idRol = usuarioRolEdit.idRol;
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            using (var db = new inventario2021Entities())
+            {
+                usuariorol usuarioRol = db.usuariorol.Find(id);
+                return View(usuarioRol);
+            }
+        }
+
+    }
+}
