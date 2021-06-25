ea781694b1e2928f287061962464a07f440b8780
franciscoariasp <francisco.ariaspri@gmail.com>
2021-06-25 02:14:23 -0500

    Creando cargar archivo"UploadCSV"Crud Proveedor

--- ProyectoAdsi/Controllers/ProveedorController.cs
+++ ProyectoAdsi/Controllers/ProveedorController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoAdsi.Models;
+using System.IO;
 
 namespace ProyectoAdsi.Controllers
 {
@@ -45,9 +46,7 @@ namespace ProyectoAdsi.Controllers
                 return View();
             }
         }
-        
-        
-
+                
         public ActionResult Edit(int id)
         {
             try
@@ -115,7 +114,69 @@ namespace ProyectoAdsi.Controllers
                 }
 
             }
+
+        public ActionResult uploadCSV()
+        {
+            return View();
         }
-    }
+
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+            //condicion para saber si llego el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpera que guardara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //condicion para saber si  la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //obtener el nomnre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+
+                
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+
+                //guardar el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                foreach (string row in csvData.Split('\n'))
+                { 
+
+                if (!string.IsNullOrEmpty(row))
+                    {
+                        var newProveedor = new proveedor
+                        {
+                            nombre = row.Split(';')[0],
+                            direccion = row.Split(';')[1],
+                            telefono = row.Split(';')[2],
+                            nombre_contacto = row.Split(';')[3],
+                        };
+                        using (var db = new inventario2021Entities())
+                        {
+                            db.proveedor.Add(newProveedor);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+
+            }
+            return View();
+                }
+            }
+        }
+    
+  
 
 
